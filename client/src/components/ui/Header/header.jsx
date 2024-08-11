5dd3e9f67bb704140ade0cf01bb2666b150aed5b
viz-zie <thisisvishkarthik@gmail.com>
2024-08-11 17:57:09 +0530

    aligned header items

--- client/src/components/ui/Header/header.jsx
+++ client/src/components/ui/Header/header.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Lottie from 'lottie-react'
-
+import React from 'react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from "@/components/ui/toggle"
@@ -30,6 +30,7 @@ import {
     CommandSeparator,
     CommandShortcut,
   } from "@/components/ui/command"
+  
 import { SheetDemo } from '@/components/ui/sidenotificationbar'
 import { DrawerDemo } from '@/components/ui/chatbotpage'
 import Image from 'next/image'
@@ -39,38 +40,50 @@ import headericon from '@/components/assets/headerlogo.png'
 
 
   export function MenubarDemo() {
+
+    
+
+   
     return (
       <div>
 
       <Menubar className="p-8 w-full align-center  ">
       <div className='flex w-1/2 sm'>
-      <MenubarMenu>
-      
-      {/*<Lottie animationData={animationdata} className='w-10 h-10'></Lottie>*/}
-      <Image src={headericon} width={40} height={40} alt='mainlogo'></Image>
-      
-        <MenubarTrigger><Link href="/">Home</Link></MenubarTrigger>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger><Link href="/servicespage">Services</Link></MenubarTrigger>
-      </MenubarMenu>
         <MenubarMenu>
+          <MenubarMenu>
+          {/*<Lottie animationData={animationdata} className='w-10 h-10'></Lottie>*/}
+          <Image src={headericon} width={40} height={40} alt='mainlogo'></Image>
+          </MenubarMenu>
+
+          <MenubarMenu>
+          <MenubarTrigger><Link href="/" >Home</Link></MenubarTrigger>
+          </MenubarMenu>
+
+          <MenubarMenu>
+            <MenubarTrigger><Link href="/servicespage">Services</Link></MenubarTrigger>
+          </MenubarMenu>
+
+          <MenubarMenu>
           <MenubarTrigger>About Us</MenubarTrigger>
+          </MenubarMenu>
+
           <MenubarMenu>
           <MenubarTrigger><Link href="/vendorpage">Vendor</Link></MenubarTrigger>
           </MenubarMenu>
+          
           <MenubarMenu>
           <ModeToggle/>
           </MenubarMenu>
+
         </MenubarMenu>
       </div>
 
-      <MenubarMenu>
-      <div className=" container ml-auto flex items-center gap-4 justify-end">
-          <SheetDemo className="flex items-center justify-end"/>
-          <Link href='./loginpage'><Button>Login / Signup</Button></Link>
-      </div>
-      </MenubarMenu>
+        <MenubarMenu>
+        <div className=" container ml-auto flex items-center gap-4 justify-end">
+            <SheetDemo className="flex items-center justify-end"/>
+            <Link href='./loginpage'><Button>Login / Signup</Button></Link>
+        </div>
+        </MenubarMenu>
       </Menubar>
       
           
