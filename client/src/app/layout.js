0fb48ed0ac59940f90a02b5f967203d2439b30e9
viz-zie <thisisvishkarthik@gmail.com>
2024-08-11 19:34:31 +0530

    finalised changes

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { MenubarDemo } from '@/components/ui/Header/header'
+import { MenubarDemo } from '@/components/ui/header'
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner"
 import Footer from "@/components/ui/footer";
