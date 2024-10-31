05c601d378a5c9c43e9094a071017a463a2fa6e8
viz-zie <thisisvishkarthik@gmail.com>
2024-10-31 12:41:31 +0530

    changed app name

--- src/components/ui/contentseparatorleft.jsx
+++ src/components/ui/contentseparatorleft.jsx
@@ -6,7 +6,7 @@ export function SeparatorDemo() {
   return (
     <div>
       <div className="space-y-5 ">
-        <h4 className="text-7xl font-bold leading-none animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">HealthMed.io</h4>
+        <h4 className="text-7xl font-bold leading-none animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">Medisoter.io</h4>
         <p className="text-lg text-muted-foreground">
           The Medical AI-Assistant
         </p>
