46fab895cbc091746678259c72bdf204e1fe1734
viz-zie <thisisvishkarthik@gmail.com>
2024-11-02 13:28:17 +0530

    finalised signup page

--- src/app/(auth)/loginsignup/page.js
+++ src/app/(auth)/loginsignup/page.js
@@ -178,7 +178,7 @@ export default function Loginpage()
               <Input id="Username" type="text" placeholder="Enter your username" value={user.username} onChange={(e) => setUser({...user,username:e.target.value})} />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="Email">Username</Label>
+              <Label htmlFor="Email">Email</Label>
               <Input id="Email" type="text" placeholder="Enter your email" value={user.email} onChange={(e) => setUser({...user,email:e.target.value})} />
             </div>
             <div className="flex flex-col space-y-1.5">
