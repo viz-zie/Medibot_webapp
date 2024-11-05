fef257555c9ace6b0f2d23a5c8da1d2ad84ea7db
viz-zie <thisisvishkarthik@gmail.com>
2024-11-05 21:17:34 +0530

    working on middleware.js

--- src/helpers/getDataFromToken.js
+++ src/helpers/getDataFromToken.js
@@ -4,8 +4,9 @@ import  jwt  from "jsonwebtoken";
 export const getDataFromToken = (request) =>{
     try
     {
-        const token = request.cookies.get("token")?.value || '';
+        const token = request.cookies.get("mytoken")?.value || '';
         const decodedToken = jwt.verify(token,process.env.TOKEN_SECRET);
+        console.log('decoded',decodedToken);
         return decodedToken.id;
     }
     catch(error)
